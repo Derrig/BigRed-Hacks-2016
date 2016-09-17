6e65256e5e5d8d215cd59faae206b416a7781499
Derrig <derrig@live.com>
2016-09-17 19:49:17 -0400

    seperated into js file

---
+++ static/heatMap.js
@@ -0,0 +1,20 @@
+function initMap() {
+  var heatMapData;
+  var map;
+  var heatmap;
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 42.4472051, lng: -76.48297680000002},
+    zoom: 15
+  });
+  heatMapData = [
+    <!--PSB-->
+    {location: new google.maps.LatLng(42.4498226, -76.481841), weight: 5},
+    <!--GATES-->
+    {location: new google.maps.LatLng(42.4448765, -76.4808143), weight: 5}
+  ];
+  heatmap = new google.maps.visualization.HeatmapLayer({
+            data: heatMapData,
+            radius: 50
+  });
+  heatmap.setMap(map);
+}
