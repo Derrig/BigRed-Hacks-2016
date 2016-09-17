a9fcd16b403df24eb0d27f5125a3db30c2f10435
Derrig <derrig@live.com>
2016-09-17 19:54:54 -0400

    gitignore

---
+++ static/heatmapData.js
@@ -0,0 +1,7 @@
+var data
+data = [
+  <!--PSB-->
+  {location: new google.maps.LatLng(42.4498226, -76.481841), weight: 5},
+  <!--GATES-->
+  {location: new google.maps.LatLng(42.4448765, -76.4808143), weight: 5}
+];
