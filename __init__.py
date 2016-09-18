d11b64b10a12d88f4cc3adb0eb2b691d81992823
Byungchan Lim <byungchanlim@Byungchans-MacBook-Pro.local>
2016-09-18 03:21:24 -0400

    Fixed map.js to work with larger datasets

--- __init__.py
+++ __init__.py
@@ -10,24 +10,14 @@ def index():
     # return ''.join('{}{}'.format(key, val) for key, val in sorted(data_fetcher.main().items()))
     id = 'homepage'
     value = cache.get(id)
+    electricity=data_fetcher.main()
     if value is not None:
         print "used cache -----------------------------"
         return value
     print 'no cache ------------------------'
-    value = render_template('index.html')
+    value = json.dump(render_template('index.html', electricity=electricity))
     cache.set(id, value, timeout=60 * 5)
     return value
-
-@app.route("/ohGod")
-def tester():
-    return "what are we doing"
-
-@app.route("/secret")
-def secret():
-    """TODO: Docstring for secret.
-    :returns: TODO
-    """
-    return 'This is a secret page. Congratulations for finding this page! Please contact Byungchan Lim at bl458@cornell.edu, and (408)-203-0543 for your prize!'
 
 if __name__ == "__main__":
     app.run()
