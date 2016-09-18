d17049a812c122209429aaa15a66baa45629d63d
Yuhuan Qiu <yqiu58@gmail.com>
2016-09-17 21:49:14 -0400

    added cache system

--- __init__.py
+++ __init__.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+import data_fetcher
+from werkzeug.contrib.cache import SimpleCache
 
+cache = SimpleCache()
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    # return ''.join('{}{}'.format(key, val) for key, val in sorted(data_fetcher.main().items()))
+    id = 'homepage'
+    value = cache.get(id)
+    if value is not None:
+        print "used cache -----------------------------"
+        return value
+    print 'no cache ------------------------'
+    value = render_template('index.html')
+    cache.set(id, value, timeout=60 * 5)
+    return value
 
 @app.route("/ohGod")
 def tester():
